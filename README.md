6e6dfc9d6b45ed0b19ccf56b0281f2c322c6e2d6
velshletter <velshletter@gmail.com>
2025-07-30 10:35:45 +0300

    chore: setup Dockerfile and docker-compose for containerized build

---
+++ README.md
@@ -0,0 +1,66 @@
+# Инструкция по запуску
+
+## Запуск системы
+
+1. **Клонировать репозиторий**
+
+```bash
+git clone https://github.com/velshletter/krainet-system.git
+cd krainet-system
+```
+
+2. **Создать `.env` файл**
+
+```bash
+cp .env.example .env
+```
+
+Заполнить переменные в `.env`:
+
+```env
+DB_USERNAME=db_user
+DB_PASSWORD=db_password
+JWT_SECRET=your_secure_secret
+
+MAIL_USERNAME=example@gmail.com
+MAIL_PASSWORD=email_app_password
+```
+
+3. **Собрать JAR-файлы (если не используются заранее собранные)**
+
+```bash
+
+mvn clean package -DskipTests
+
+```
+
+4. **Запустить систему**
+
+```bash
+docker-compose up --build
+```
+
+**Тестовые данные:**
+(пароль для админа: Admin-123, пароль для юзера: User-123)
+```json
+[
+  {
+    "id": "11111111-1111-1111-1111-111111111111",
+    "username": "admin",
+    "password": "$2a$12$3ousmAokL7cagKqtP3lcGOfRWTWhGS3px/LiSSp779duA/vWGZJH2",
+    "email": "admin@example.com",
+    "first_name": "Admin",
+    "last_name": "User",
+    "role": "ADMIN"
+  },
+  {
+    "id": "22222222-2222-2222-2222-222222222222",
+    "username": "john_doe",
+    "password": "$2a$12$SK20UxrEQsGAKuCvDOrTzu9YKXtuJQLzGeFCROfmXh.N9o09Ktxii",
+    "email": "user@example.com",
+    "first_name": "John",
+    "last_name": "Doe",
+    "role": "USER"
+  }
+]
+```
--- auth-service/src/main/resources/db/migration/V1__init.sql
+++ auth-service/src/main/resources/db/migration/V1__init.sql
@@ -1,11 +1,36 @@
 CREATE TYPE role_enum AS ENUM ('USER', 'ADMIN');
 
-CREATE TABLE users (
-    id UUID PRIMARY KEY,
-    username VARCHAR(50) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
+CREATE TABLE users
+(
+    id         UUID PRIMARY KEY,
+    username   VARCHAR(50) UNIQUE  NOT NULL,
+    password   VARCHAR(255)        NOT NULL,
+    email      VARCHAR(100) UNIQUE NOT NULL,
     first_name VARCHAR(50),
-    last_name VARCHAR(50),
-    role VARCHAR(50) NOT NULL
-); 
+    last_name  VARCHAR(50),
+    role       VARCHAR(50)         NOT NULL
+);
+
+CREATE TABLE refresh_tokens
+(
+    id         UUID PRIMARY KEY,
+    user_id    UUID                        NOT NULL,
+    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    expires_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    CONSTRAINT fk_refresh_token_user FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE INDEX idx_refresh_token_user_id ON refresh_tokens (user_id);
+
+INSERT INTO users (id, username, password, email, first_name, last_name, role)
+VALUES
+    ('11111111-1111-1111-1111-111111111111', 'admin', '$2a$14$ygS/eP43CRg5a/chM9vDZu.Src07WpgFHctHYiY/VhFWQycyo0pXW', 'admin@example.com', 'Admin', 'User', 'ADMIN'),
+    ('22222222-2222-2222-2222-222222222222', 'john_doe', '$2a$12$TCIm9MTwX85Pjsz26..PJ.hluh9zuoWUiuv6HdvfJ5oEcB7pDOo/K', 'user@example.com', 'John', 'Doe', 'USER');
+
+INSERT INTO refresh_tokens (id, user_id, created_at, expires_at)
+VALUES (
+           '33333333-3333-3333-3333-333333333333',
+           '22222222-2222-2222-2222-222222222222',
+           NOW(),
+           NOW() + INTERVAL '30 days'
+       );
